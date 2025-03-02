89d6441ef6be8071c130eed845b4d350bb7dcb34
Matthieu Burban <matthieuburban@MacBook-Pro-de-Matthieu.local>
2025-03-02 17:04:52 -0500

    add profile picture for results page

--- voter-app/src/components/Vote/VoteResults.tsx
+++ voter-app/src/components/Vote/VoteResults.tsx
@@ -1,6 +1,7 @@
 // src/components/Vote/VoteResults.tsx
 import React from 'react';
 import { Result } from '../../types';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 interface VoteResultsProps {
   results: Result[];
@@ -10,13 +11,24 @@ const VoteResults: React.FC<VoteResultsProps> = ({ results }) => {
   return (
     <div>
       <h2>Voting Results</h2>
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>
-            Candidate {result.candidate_id} /{result.vote_type}: {result.vote_count} votes
-          </li>
+      <Container>
+      <Row>
+        {results.map(result => (
+          <Col key={result.candidate_id} md={4} className="mb-4">
+            <Card>
+              <Card.Img variant="top" src={`../profil_picture.png`} />
+              <Card.Body>
+                <Card.Title>{result.candidate_id}</Card.Title>
+                <Card.Text>
+                  Vote Type: {result.vote_type}<br />
+                  Votes: {result.vote_count}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ul>
+      </Row>
+    </Container>
     </div>
   );
 };
