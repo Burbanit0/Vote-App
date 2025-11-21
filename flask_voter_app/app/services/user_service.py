8bf5d4f6bf7de65255200c2b46dda5ec548a1f6f
Burbanit0 <gaultier.burban@gmail.com>
2025-11-21 18:37:23 +0100

    solve issues

--- flask_voter_app/app/services/user_service.py
+++ flask_voter_app/app/services/user_service.py
@@ -49,7 +49,7 @@ class UserService:
             "role": user.role,
             "first_name": user.first_name,
             "last_name": user.last_name,
-        }, 200
+        }
 
     @staticmethod
     def get_profile(user_id):
